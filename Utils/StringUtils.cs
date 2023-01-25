27331d31d71fd15cb8d16beec3801872ac30d377
Amber-Eimer-Student <112406480+Amber-Eimer-Student@users.noreply.github.com>
2023-01-25 14:56:17 +0000

    Mess

--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -2,21 +2,61 @@ namespace MyUtilities
 {
     public static class StringUtils
     {
+        public static string ascii(string s)
+        {
+            string s = Console.ReadLine();
+            foreach (char c in s)
+            {
+                System.Convert.ToInt32(c);
+                int unicode = c;
+                char character = (char)unicode;
+                string text = character.ToString();
+                Console.Write(text);
+            }
+        }
         public static string ToUpper(string s)
         {
             string s = Console.ReadLine();
             foreach (char c in s)
             {
                 System.Convert.ToInt32(c);
-                int unicode = c - 32;
-                char character = (char)unicode;
-                string text = character.ToString();
-                Console.WriteLine(text);
+                if (c >= 97 && c <= 122)
+                {
+                    int unicode = c - 32;
+                    char character = (char)unicode;
+                    string text = character.ToString();
+                    Console.WriteLine(text);
+                }
+                else
+                {
+                    int unicode = c;
+                    char character = (char)unicode;
+                    string text = character.ToString();
+                    Console.WriteLine(text);
+                }
             }
         }
         public static string ToLower(string s)
         {
-
+            string s = Console.ReadLine();
+            foreach (char c in s)
+            {
+                System.Convert.ToInt32(c);
+                if (c >= 65 && c <= 90)
+                {
+                    int unicode = c + 32;
+                    char character = (char)unicode;
+                    string text = character.ToString();
+                    Console.Write(text);
+                }
+                else
+                {
+                    int unicode = c;
+                    char character = (char)unicode;
+                    string text = character.ToString();
+                    Console.Write(text);
+                }
+            }
         }
         public static string length(string s)
         {
