0348b1c00c68a9ab246c8aa1c4e7548aa42594ad
Amber-Eimer-Student <112406480+Amber-Eimer-Student@users.noreply.github.com>
2023-01-25 14:27:56 +0000

    I dont know how to do classes. Worked in practise.

--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -2,11 +2,23 @@ namespace MyUtilities
 {
     public static class StringUtils
     {
-        public static string ToUpper()
+        public static string ToUpper(string s)
         {
-        
+            string s = Console.ReadLine();
+            foreach (char c in s)
+            {
+                System.Convert.ToInt32(c);
+                int unicode = c - 32;
+                char character = (char)unicode;
+                string text = character.ToString();
+                Console.WriteLine(text);
+            }
         }
-        public static string ToLower()
+        public static string ToLower(string s)
+        {
+
+        }
+        public static string length(string s)
         {
 
         }
