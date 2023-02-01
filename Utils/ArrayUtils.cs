e14213eaf9e51fe688f9f164561711b4d4e32dea
Luc Pepper <112406951+Luc-Pepper-Student@users.noreply.github.com>
2023-02-01 13:59:43 +0000

    Jacob will work on this

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -1,6 +1,103 @@
-namespace MyUtilities {
-    
-    public static class ArrayUtils {
+namespace MyUtilities
+{
+
+    public static class ArrayUtils
+    {
+        /// <summary>
+        /// Finds the Average of the Array
+        /// </summary>
+        /// <array name ="a"> The array
+        /// <returns>
+        /// The average of array a
+        /// </returns>
+
+        public static int Average(int[] a)
+        {
+            int b = a[0] + a[1] + a[2] + a[3];
+            b = b / a.Length;
+            return b;
+        }
+        public static int Max(int[] a)
+        {
+            int x = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[x] > a[i])
+                {
+                    return a[x];
+                }
+                else if (a[x] < a[i])
+                {
+                    x = i;
+                }
+                else
+                {
+                    return a[x];
+                }
+            }
+            return a[x];
+        }
+        public static int Min(int[] a)
+        {
+            int x = 0;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[x] < a[i])
+                {
+                    return a[x];
+                }
+                else if (a[x] > a[i])
+                {
+                    x = i;
+                }
+                else
+                {
+                    return a[x];
+                }
+            }
+            return a[x];
+        }
+        //public static int SortAscending(int[] a)
+        // {
+        //     int i = 0;
+        //     int n = 4;
+        //     for ( i = 0; i < n - 1; i++)
+        //         for (int j = 0; j < n - i - 1; j++)
+        //             if (a[j] > a[j + 1])
+        //             {
+        //                 a[i] = a[j];
+        //                 a[j] = a[j + 1];
+        //                 a[j + 1] = a[i];
+        //             }
+        //             return a[i];
+        // }
+        public static int Sum(int[] a)
+        {
+            int x = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                x = x + a[i];
+            }
+            return x;
+        }
+        public static int Product(int[] a)
+        {
+            int x = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                x = x * a[i];
+            }
+            return x;
+        }
+        public static int[] Reverse(int[] a)
+        {
+            int[] arr = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                arr[i] = a[(a.Length - 1) - i];
+            }
+            return arr;
+        }
 
     }
 }
--- Utils/Program.cs
+++ Utils/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using MyUtilities;
+public class Program
+{
+    public static void Main(string[] arg)
+    {
+        int[] a = { 5, 15, 20, 30 };
+        int resultAver = ArrayUtils.Average(a);
+        Console.WriteLine("The Average is: " + resultAver);
+
+        int resultMax = ArrayUtils.Max(a);
+        Console.WriteLine("The Max is: " + resultMax);
+
+        int resultMin = ArrayUtils.Min(a);
+        Console.WriteLine("The Min is: " + resultMin);
+
+        // int resultSortA = ArrayUtils.SortAscending(a);
+        // Console.WriteLine(resultSortA);
+
+        int resultSum = ArrayUtils.Sum(a);
+        Console.WriteLine("The Sum is: " + resultSum);
+
+        int resultProd = ArrayUtils.Product(a);
+        Console.WriteLine("The Product is: " + resultProd);
+        
+        int [] resultRev = ArrayUtils.Reverse(a);
+        Console.Write("The array in Reverse is: ");
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.Write(resultRev[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
